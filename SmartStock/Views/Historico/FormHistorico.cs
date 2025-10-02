0e4d405e60cd8daa570eb0c87d184e4024b8a017
Bernardo Coelho Simões <bernardo.coelhosimoes@gmail.com>
2025-10-02 18:04:20 -0300

    02/10

--- SmartStock/Views/Historico/FormHistorico.cs
+++ SmartStock/Views/Historico/FormHistorico.cs
@@ -37,24 +37,27 @@ namespace SmartStock.Views.Historico
 					DgvListagem?.Columns.Clear();
 				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridTextColumn() { MappingName = "IdProduto", HeaderText = "Id" });
 				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridTextColumn() { MappingName = "Nome", HeaderText = "Nome" });
-				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridTextColumn() { MappingName = "QuantidadeAtual", HeaderText = "Quantidade" });
+				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridTextColumn() { MappingName = "QuantidadeAtual", HeaderText = "Quantidade Atual" });
 				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridTextColumn() { MappingName = "Preco", HeaderText = "Preço" });
 				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridDateTimeColumn() { MappingName = "Validade", HeaderText = "Data Validade" });
 				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridTextColumn() { MappingName = "Descricao", HeaderText = "Descricao" });
 				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridTextColumn() { MappingName = "Status", HeaderText = "Status" });
 				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridCheckBoxColumn() { MappingName = "Ativo", HeaderText = "Ativo" });
+				DgvListagem.Columns.Add(new Syncfusion.WinForms.DataGrid.GridTextColumn() { MappingName = "QuantDoado", HeaderText = "Quantidade Doada" });
 
 				DgvListagem.DataSource = Lista(int.Parse(ComboCategoria.SelectedValue.ToString()));
 				DgvListagem.Refresh();
 			}
 		}
 
-		private List<Produto> Lista(int idCategoria)
+		private List<Models.Partials.Produto> Lista(int idCategoria)
 		{
-			var lista = new List<Produto>();
+			var lista = new List<Models.Partials.Produto>();
 			string query = "SELECT idProduto, nome, quantidadeAtual, preco, " +
-			"validade, preco, descricao, status, ativo " +
-			"FROM Produto WHERE idEmpresa = @idEmpresa AND idCategoria = @idCategoria";
+			"validade, descricao, status, ativo, SUM(quantidade) " +
+			"FROM Produto p " +
+			"LEFT JOIN Doacao d USING(idProduto) WHERE idEmpresa = @idEmpresa AND idCategoria = @idCategoria " +
+			"GROUP BY p.idProduto";
 			DataTable dt = FormLogin.bd.ExecutarConsulta(query, new List<MySql.Data.MySqlClient.MySqlParameter>()
 			{
 				new MySql.Data.MySqlClient.MySqlParameter("@idEmpresa", FormPrincipal._empresa.IdEmpresa),
@@ -63,10 +66,10 @@ namespace SmartStock.Views.Historico
 
 			foreach (DataRow row in dt.Rows)
 			{
-				Produto produto = new Produto();
+				Models.Partials.Produto produto = new Models.Partials.Produto();
 				produto.IdProduto = row.IsNull("idProduto") ? 0 : row.Field<int>("idProduto");
 				produto.Descricao = row.IsNull("descricao") ? string.Empty : row.Field<string>("descricao");
-				produto.Quantidade = row.IsNull("quantidadeAtual") ? 0 : row.Field<int>("quantidadeAtual");
+				produto.QuantidadeAtual = row.IsNull("quantidadeAtual") ? 0 : row.Field<int>("quantidadeAtual");
 				produto.Ativo = row.IsNull("ativo") ? false : Convert.ToBoolean(row["ativo"]);
 				produto.Nome = row.IsNull("nome") ? string.Empty : row.Field<string>("nome");
 				produto.Preco = row.IsNull("preco") ? 0.0m : row.Field<decimal>("preco");
